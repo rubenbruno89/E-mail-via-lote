92e613337bbe1c813dd08a8c766017d9764a8f3a
Ruben Bruno Cavalcante de Menezes <34634888+rubenbruno89@users.noreply.github.com>
2023-05-04 17:24:28 -0300

    Update email.bat

--- email.bat
+++ email.bat
@@ -3,6 +3,4 @@ set /p to=Digite o endereço de e-mail do destinatário:
 set /p subject=Digite o assunto do e-mail:
 set /p body=Digite o conteúdo do e-mail:
 
-blat -to %to% -subject "%subject%" -body "%body%" -server smtp-mail.outlook.com
-
- -port 587 -u rubenbruno@outlook.com -pw rbcm86256650 -f rubenbrumo@outlook.com
+blat -to %to% -subject "%subject%" -body "%body%" -server <SMTP_SERVER> -port <SMTP_PORT> -u <SMTP_USERNAME> -pw <SMTP_PASSWORD> -f <SENDER_EMAIL>
